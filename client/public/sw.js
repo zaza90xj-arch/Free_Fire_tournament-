dda08155848ccfb53216f1ea24be050063d2f0f1
zaza90xj-arch <zaza90xj@gmail.com>
2026-09-04 00:59:37 +0300

    Add files via upload

---
+++ client/public/sw.js
@@ -0,0 +1,10 @@
+// Service Worker للإشعارات
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/logo.png'
+    })
+  );
+});
